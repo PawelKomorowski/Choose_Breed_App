4e08ec746281d548bd9326f1aa010aa53c696ed2
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-15 16:34:57 +0100

    Enable JPA repositories

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/BackendApplication.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package chooseBreed.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "chooseBreed.backend.db.repositories")
 @SpringBootApplication
 public class BackendApplication {
 
