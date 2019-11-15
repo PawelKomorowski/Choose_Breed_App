91111464dd953f60bd49801a96151538dfac1468
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-15 20:15:55 +0100

    Add toString() method and delete unnecessary imports

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/model/view/BreedsInfo.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/model/view/BreedsInfo.java
@@ -3,28 +3,18 @@ package chooseBreed.backend.db.model.view;
 import chooseBreed.backend.db.model.enums.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Subselect;
-import org.hibernate.annotations.Synchronize;
 import org.hibernate.annotations.Type;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "breeds_info")
-//@Subselect(
-//        "SELECT b.name, g.name as group_name, b.size, b.weight, i.illnesses_count, b.cleaning_difficulty, b.train_difficulty, b.hair_length, b.hair_type, b.live_length, b.cost, b.livelihood_cost, b.description\n" +
-//                "FROM breed b \n" +
-//                "INNER JOIN breed_group g ON b.breed_group_id = g.id\n" +
-//                "INNER JOIN photo p ON b.name = p.breed_name\n" +
-//                "INNER JOIN (SELECT breeds_name, COUNT(*) as illnesses_count FROM breed_illnesses GROUP BY breeds_name) i ON i.breeds_name = b.name"
-//)
-//@Synchronize({"breed", "breed_group", "photo", "breed_illnesses"})
 @Immutable
 @Getter
 @NoArgsConstructor
+@ToString
 public class BreedsInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
