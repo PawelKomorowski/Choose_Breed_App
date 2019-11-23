962cee188b3764f2d404e322a2fe63d2c2233b1c
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-23 18:13:19 +0100

    Change search according to illness changes

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/repositories/BreedInfoRepository.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/repositories/BreedInfoRepository.java
@@ -13,10 +13,14 @@ public interface BreedInfoRepository extends JpaRepository<BreedsInfo, String> {
     List<BreedsInfo> findByName(String name);
 
     @Query(value = "SELECT b FROM BreedsInfo b WHERE b.size IN (:size) OR b.cleaningDifficulty IN (:cleaningDifficulty) " +
+            "OR b.illnessPossibility IN (:illnessPossibility) " +
             "OR b.trainDifficulty IN (:trainingDifficulty) " +
             "OR b.hairLength IN (:hairLength) " +
             "OR b.hairType IN (:hairType)")
-    List<BreedsInfo> findByParams(@Param("size") Collection<Size> size, @Param("cleaningDifficulty") Collection<CleaningDifficulty> cleaningDifficulty,
-                                  @Param("trainingDifficulty") Collection<TrainDifficulty> trainingDifficulty, @Param("hairLength") Collection<HairLength> hairLength,
+    List<BreedsInfo> findByParams(@Param("size") Collection<Size> size,
+                                  @Param("illnessPossibility") Collection<IllnessPossibility> illnessPossibility,
+                                  @Param("cleaningDifficulty") Collection<CleaningDifficulty> cleaningDifficulty,
+                                  @Param("trainingDifficulty") Collection<TrainDifficulty> trainingDifficulty,
+                                  @Param("hairLength") Collection<HairLength> hairLength,
                                   @Param("hairType") Collection<HairType> hairType);
 }
