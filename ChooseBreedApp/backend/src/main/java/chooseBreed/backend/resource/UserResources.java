c2c7e1e6b974e9a4fee70c9c251b96a6a970e8d0
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-23 20:28:08 +0100

    Move casting params methods to util.Parameters

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/UserResources.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/UserResources.java
@@ -14,6 +14,9 @@ import java.util.Collection;
 import java.util.List;
 
 
+import static chooseBreed.backend.resource.util.Parameters.*;
+
+
 @Controller
 public class UserResources {
     final BreedInfoRepository breedInfoRepository;
@@ -70,133 +73,5 @@ public class UserResources {
         model.addAttribute("pageTitle", "Wyniki wyszukiwania");
 
         return "result";
-    }
-
-    private Collection<Size> stringToSize(List<String> size){
-        Collection<Size> collection = new ArrayList<>();
-        if(size == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : size){
-            switch (s){
-                case "maly": collection.add(Size.MALY);
-                    break;
-                case "sredni": collection.add(Size.SREDNI);
-                    break;
-                case "duzy": collection.add(Size.DUZY);
-                    break;
-                case "olbrzym": collection.add(Size.OLBRZYM);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
-    }
-
-    private Collection<CleaningDifficulty> stringToCleaningDifficulty(List<String> cleaningDifficulty){
-        Collection<CleaningDifficulty> collection = new ArrayList<>();
-        if(cleaningDifficulty == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : cleaningDifficulty){
-            switch (s){
-                case "latwo": collection.add(CleaningDifficulty.LATWO);
-                    break;
-                case "sredno": collection.add(CleaningDifficulty.SREDNIO);
-                    break;
-                case "trudno": collection.add(CleaningDifficulty.TRUDNO);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
-    }
-
-    private Collection<TrainDifficulty> stringToTrainDifficulty(List<String> trainDifficulty){
-        Collection<TrainDifficulty> collection = new ArrayList<>();
-        if(trainDifficulty == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : trainDifficulty){
-            switch (s){
-                case "latwo": collection.add(TrainDifficulty.LATWO);
-                    break;
-                case "sredno": collection.add(TrainDifficulty.SREDNIO);
-                    break;
-                case "trudno": collection.add(TrainDifficulty.TRUDNO);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
-    }
-
-    private Collection<HairLength> stringToHairLength(List<String> hairLength){
-        Collection<HairLength> collection = new ArrayList<>();
-        if(hairLength == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : hairLength){
-            switch (s){
-                case "krotka": collection.add(HairLength.KROTKA);
-                    break;
-                case "srednia": collection.add(HairLength.SREDNIA);
-                    break;
-                case "dluga": collection.add(HairLength.DLUGA);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
-    }
-
-    private Collection<HairType> stringToHairType(List<String> hairType){
-        Collection<HairType> collection = new ArrayList<>();
-        if(hairType == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : hairType){
-            switch (s){
-                case "puchata": collection.add(HairType.PUCHATA);
-                    break;
-                case "szorstka": collection.add(HairType.SZORSTKA);
-                    break;
-                case "gladka": collection.add(HairType.GLADKA);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
-    }
-
-    private Collection<IllnessPossibility> stringToIllnessPossibility(List<String> illnessPossibility){
-        Collection<IllnessPossibility> collection = new ArrayList<>();
-        if(illnessPossibility == null){
-            collection.add(null);
-            return collection;
-        }
-
-        for(String s : illnessPossibility){
-            switch (s){
-                case "mala": collection.add(IllnessPossibility.MALA);
-                    break;
-                case "srednia": collection.add(IllnessPossibility.SREDNIA);
-                    break;
-                case "duza": collection.add(IllnessPossibility.DUZA);
-                    break;
-                default: break;
-            }
-        }
-        return collection;
     }
 }
