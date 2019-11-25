1c689e80a415bc4b5d322ac4a861a26b5def5726
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-25 15:43:11 +0100

    Working on results

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/UserResources.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/UserResources.java
@@ -2,27 +2,36 @@ package chooseBreed.backend.resource;
 
 import chooseBreed.backend.db.model.enums.*;
 import chooseBreed.backend.db.model.view.BreedsInfo;
-import chooseBreed.backend.db.repositories.BreedInfoRepository;
+import chooseBreed.backend.db.repositories.*;
+import chooseBreed.backend.resource.util.AssessResult;
+import chooseBreed.backend.resource.wrappers.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 
-import static chooseBreed.backend.resource.util.Parameters.*;
+import static chooseBreed.backend.resource.util.CastParameters.*;
 
 
 @Controller
 public class UserResources {
     final BreedInfoRepository breedInfoRepository;
+    final CostRepository costRepository;
+    final LiveLengthRepository liveLengthRepository;
+    final LivelihoodCostRepository livelihoodCostRepository;
+    final WeightRepository weightRepository;
 
-    public UserResources(BreedInfoRepository breedInfoRepository) {
+    public UserResources(BreedInfoRepository breedInfoRepository, CostRepository costRepository, LiveLengthRepository liveLengthRepository, LivelihoodCostRepository livelihoodCostRepository, WeightRepository weightRepository) {
         this.breedInfoRepository = breedInfoRepository;
+        this.costRepository = costRepository;
+        this.liveLengthRepository = liveLengthRepository;
+        this.livelihoodCostRepository = livelihoodCostRepository;
+        this.weightRepository = weightRepository;
     }
 
     @GetMapping("/all")
@@ -62,14 +71,26 @@ public class UserResources {
                          @RequestParam(required = false) List<String> cleaning_difficulty, @RequestParam(required = false) List<String> training_difficulty,
                          @RequestParam(required = false) List<String> hair_length, @RequestParam(required = false) List<String> hair_type){
 
-        List<BreedsInfo> breedsInfos = breedInfoRepository.findByParams(stringToSize(size), stringToIllnessPossibility(illnesses),
-                stringToCleaningDifficulty(cleaning_difficulty), stringToTrainDifficulty(training_difficulty),
-                stringToHairLength(hair_length), stringToHairType(hair_type));
+        Collection<Size> sizeParam = stringToSize(size);
+        Collection<IllnessPossibility> illnessParam = stringToIllnessPossibility(illnesses);
+        Collection<CleaningDifficulty> cleaningParam = stringToCleaningDifficulty(cleaning_difficulty);
+        Collection<TrainDifficulty> trainParam = stringToTrainDifficulty(training_difficulty);
+        Collection<HairLength> hairLengthParam = stringToHairLength(hair_length);
+        Collection<HairType> hairTypeParam = stringToHairType(hair_type);
 
-        for(BreedsInfo b : breedsInfos)
-            System.out.println(b.toString());
 
-        model.addAttribute("results", breedsInfos);
+        List<BreedsInfo> breedsInfos = breedInfoRepository.findByParams(sizeParam, illnessParam, cleaningParam,
+                trainParam, hairLengthParam, hairTypeParam);
+
+        List<Result> results = new AssessResult().assess(breedsInfos,  size, illnessParam, live_length, cost,
+                livelihood_cost, cleaningParam, trainParam, hairLengthParam, hairTypeParam,
+                castLiveLengthParams(liveLengthRepository.findAll()), castCostParams(costRepository.findAll()),
+                castLivelihoodCostParams(livelihoodCostRepository.findAll()), castWeightParams(weightRepository.findAll()));
+
+        for(Result r : results)
+            r.calculatePercentageScore();
+
+        model.addAttribute("results", results);
         model.addAttribute("pageTitle", "Wyniki wyszukiwania");
 
         return "result";
--- ChooseBreedApp/backend/src/main/resources/templates/result.ftl
+++ ChooseBreedApp/backend/src/main/resources/templates/result.ftl
@@ -8,12 +8,12 @@
             <!-- <div class="card__image card__image--right"> -->
             <!--     <img class="card__img" src="" alt=""> -->
             <!-- </div> -->
-            <h3 class="card__title">${result.name}</h3>
+            <h3 class="card__title">${result.breedsInfo.name} - ${result.percentageScore}%</h3>
             <div class="card__content">
-                ${result.description?truncate(350)}
+                ${result.breedsInfo.description?truncate(350)}
             </div>
             <div class="card__actions">
-                <a class="card__button text--teal" href="/breed/${result.urlEncode()}">Więcej</a>
+                <a class="card__button text--teal" href="/breed/${result.breedsInfo.urlEncode()}">Więcej</a>
             </div>
         </article>
     </#list>
