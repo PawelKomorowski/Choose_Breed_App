361c42e3debc2fe05e17d92518fd321216da6631
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-11-25 18:42:16 +0100

    Add `add` and `edit` admin resources

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/AdminResources.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/AdminResources.java
@@ -2,6 +2,12 @@ package chooseBreed.backend.resource;
 
 import chooseBreed.backend.db.repositories.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 
 @Controller
 public class AdminResources {
@@ -13,5 +19,35 @@ public class AdminResources {
         this.photoRepository = photoRepository;
     }
 
+    @GetMapping("/edit/{name}")
+    public String editBreed(@PathVariable String name, Model model) {
+        String url;
+        try {
+            url = URLDecoder.decode(name, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            url = name;
+        }
+
+        model.addAttribute("pageTitle", "Edycja: " + url);
+        model.addAttribute("pageAction", "Edycja");
+        model.addAttribute("breed", breedRepository.findByName(url).get(0));
+
+        return "edit";
+    }
+
+    @GetMapping("/add")
+    public String addBreed(Model model) {
+        model.addAttribute("pageTitle", "Dodawanie");
+        model.addAttribute("pageAction", "Dodawanie");
+
+        return "edit";
+    }
+
+    @PostMapping("/save")
+    public String saveBreed(Model model) {
+        // TODO: Handle incoming values
+
+        return "";
+    }
 
 }
