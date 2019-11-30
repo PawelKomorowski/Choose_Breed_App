9a066845df3e7e76dbb6c76909f1aa65c041e236
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-30 14:38:47 +0100

    Add new condition to query

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/repositories/BreedInfoRepository.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/db/repositories/BreedInfoRepository.java
@@ -18,11 +18,20 @@ public interface BreedInfoRepository extends JpaRepository<BreedsInfo, String> {
             "OR b.illnessPossibility IN (:illnessPossibility) " +
             "OR b.trainDifficulty IN (:trainingDifficulty) " +
             "OR b.hairLength IN (:hairLength) " +
-            "OR b.hairType IN (:hairType)")
+            "OR b.hairType IN (:hairType) " +
+            "OR (b.cost > :costMin AND b.cost < :costMax) " +
+            "OR (b.liveLength > :liveLengthMin AND b.liveLength < :liveLengthMax) " +
+            "OR (b.livelihoodCost > :livelihoodCostMin AND b.livelihoodCost < :livelihoodCostMax)")
     List<BreedsInfo> findByParams(@Param("size") Collection<Size> size,
                                   @Param("illnessPossibility") Collection<IllnessPossibility> illnessPossibility,
                                   @Param("cleaningDifficulty") Collection<CleaningDifficulty> cleaningDifficulty,
                                   @Param("trainingDifficulty") Collection<TrainDifficulty> trainingDifficulty,
                                   @Param("hairLength") Collection<HairLength> hairLength,
-                                  @Param("hairType") Collection<HairType> hairType);
+                                  @Param("hairType") Collection<HairType> hairType,
+                                  @Param("costMin") short costMin,
+                                  @Param("costMax") short costMax,
+                                  @Param("liveLengthMin") short liveLengthMin,
+                                  @Param("liveLengthMax") short liveLengthMax,
+                                  @Param("livelihoodCostMin") short livelihoodCostMin,
+                                  @Param("livelihoodCostMax") short livelihoodCostMax);
 }
