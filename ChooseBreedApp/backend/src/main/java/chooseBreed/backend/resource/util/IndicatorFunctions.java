aa32dbf1cbb1770b9836d9c78da39426b705551a
Paweł Komorowski <pkomorowski@gamil.com>
2019-11-30 14:33:45 +0100

    Delete comments

--- ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/util/IndicatorFunctions.java
+++ ChooseBreedApp/backend/src/main/java/chooseBreed/backend/resource/util/IndicatorFunctions.java
@@ -2,7 +2,6 @@ package chooseBreed.backend.resource.util;
 
 import chooseBreed.backend.db.model.view.BreedsInfo;
 import chooseBreed.backend.resource.wrappers.FuzzyParam;
-import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
@@ -14,21 +13,14 @@ public class IndicatorFunctions {
     private List<FuzzyParam> livelihoodCostParams;
     private List<FuzzyParam> weightParams;
 
-
-    //TODO na podstawie parametrów z wyszukiwarki określić jaka będzie funkcja i wtedy policzyć
-
-    // Sprawdza wynik dla kosztu
     public float checkCost(BreedsInfo breedsInfo, List<String> costs){
         if(costs == null || costs.size() == 0)
             return 0;
         float maxScore = 0;
         float tempScore = 0;
         FuzzyParam chosenParam;
-        //Dla każdego z zaznaczonych kosztów w wyszukiwarce
         for(String s : costs){
-            //Pobiera odpowiadający zbiór
             chosenParam = getCostParam(s);
-            //Jeśli zbiór istnieje to oblicz odpowiednią wartość funkcji i podstaw pod teymczasową wartość
             if(chosenParam != null){
                 switch (chosenParam.getFunction()){
                     case "L": tempScore = functionL(chosenParam.getA(), chosenParam.getB(), breedsInfo.getCost());
@@ -47,18 +39,14 @@ public class IndicatorFunctions {
         return maxScore;
     }
 
-    // Sprawdza wynik dla kosztu
     public float checkLiveLength(BreedsInfo breedsInfo, List<String> liveLengths){
         if(liveLengths == null || liveLengths.size() == 0)
             return 0;
         float maxScore = 0;
         float tempScore = 0;
         FuzzyParam chosenParam;
-        //Dla każdego z zaznaczonych kosztów w wyszukiwarce
         for(String s : liveLengths){
-            //Pobiera odpowiadający zbiór
             chosenParam = getLiveLengthParam(s);
-            //Jeśli zbiór istnieje to oblicz odpowiednią wartość funkcji i podstaw pod teymczasową wartość
             if(chosenParam != null){
                 switch (chosenParam.getFunction()){
                     case "L": tempScore = functionL(chosenParam.getA(), chosenParam.getB(), breedsInfo.getLiveLength());
@@ -77,18 +65,14 @@ public class IndicatorFunctions {
         return maxScore;
     }
 
-    // Sprawdza wynik dla kosztu
     public float checkLivelihoodCost(BreedsInfo breedsInfo, List<String> livelihoodCosts){
         if(livelihoodCosts == null || livelihoodCosts.size() == 0)
             return 0;
         float maxScore = 0;
         float tempScore = 0;
         FuzzyParam chosenParam;
-        //Dla każdego z zaznaczonych kosztów w wyszukiwarce
         for(String s : livelihoodCosts){
-            //Pobiera odpowiadający zbiór
             chosenParam = getLivelihoodCostParam(s);
-            //Jeśli zbiór istnieje to oblicz odpowiednią wartość funkcji i podstaw pod teymczasową wartość
             if(chosenParam != null){
                 switch (chosenParam.getFunction()){
                     case "L": tempScore = functionL(chosenParam.getA(), chosenParam.getB(), breedsInfo.getLivelihoodCost());
@@ -107,18 +91,14 @@ public class IndicatorFunctions {
         return maxScore;
     }
 
-    // Sprawdza wynik dla kosztu
     public float checkWeight(BreedsInfo breedsInfo, List<String> weights){
         if(weights == null || weights.size() == 0)
             return 0;
         float maxScore = 0;
         float tempScore = 0;
         FuzzyParam chosenParam;
-        //Dla każdego z zaznaczonych kosztów w wyszukiwarce
         for(String s : weights){
-            //Pobiera odpowiadający zbiór
             chosenParam = getWeightParam(s);
-            //Jeśli zbiór istnieje to oblicz odpowiednią wartość funkcji i podstaw pod teymczasową wartość
             if(chosenParam != null){
                 switch (chosenParam.getFunction()){
                     case "L": tempScore = functionL(chosenParam.getA(), chosenParam.getB(), breedsInfo.getWeight());
